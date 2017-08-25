a1836303e2fd5a8d8b6c94991b61f2bc6d55e2b7
melisa014 <mikhailovajk@mail.ru>
2017-08-25 19:07:06 +0300

    22

--- index.php
+++ index.php
@@ -1,8 +1,6 @@
 <html>
     <head>Тест-сайт</head>
-    <title>Yii2-test</title>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-    <link rel="shortcut icon" href="/basic/web/yiidebug1.png" type="image/png">
+
     <body>
         Hello!<br>
         <?php
