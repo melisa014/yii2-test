b59f4027ef9809b7dc8518e69055faf9b7696913
melisa014 <mikhailovajk@mail.ru>
2017-08-29 14:05:00 +0300

    работа с формами

--- basic/config/db.php
+++ basic/config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'dsn' => 'mysql:host=yii2.loc;dbname=yii2basic',
     'username' => 'root',
-    'password' => '',
+    'password' => '1234',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
---
+++ basic/models/Country.php
@@ -0,0 +1,11 @@
+<?php
+namespace app\models;
+
+use yii\db\ActiveRecord;
+
+/**
+ * Класс для работы с моделью Country (таблица country)
+ */
+class Country extends ActiveRecord
+{
+}
